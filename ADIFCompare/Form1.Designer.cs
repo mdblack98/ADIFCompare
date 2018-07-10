e4b9ac6ae179dbdc52307604caf7d269f6cee16b
Michael Black <mdblack98@yahoo.com>
2018-07-10 13:08:00 -0500

    Initial github checki

---
+++ ADIFCompare/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ADIFCompare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.run = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBoxIgnoreMode = new System.Windows.Forms.CheckBox();
+            this.checkBoxIgnoreDups = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // run
+            // 
+            this.run.Location = new System.Drawing.Point(13, 65);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(75, 23);
+            this.run.TabIndex = 5;
+            this.run.Text = "Run";
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(13, 93);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(546, 284);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(393, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(95, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(393, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(495, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(495, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(381, 65);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(107, 20);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(273, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Station Callsign Filter";
+            // 
+            // checkBoxIgnoreMode
+            // 
+            this.checkBoxIgnoreMode.AutoSize = true;
+            this.checkBoxIgnoreMode.Location = new System.Drawing.Point(95, 67);
+            this.checkBoxIgnoreMode.Name = "checkBoxIgnoreMode";
+            this.checkBoxIgnoreMode.Size = new System.Drawing.Size(85, 17);
+            this.checkBoxIgnoreMode.TabIndex = 10;
+            this.checkBoxIgnoreMode.Text = "Ignore mode";
+            this.checkBoxIgnoreMode.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxIgnoreDups
+            // 
+            this.checkBoxIgnoreDups.AutoSize = true;
+            this.checkBoxIgnoreDups.Location = new System.Drawing.Point(179, 67);
+            this.checkBoxIgnoreDups.Name = "checkBoxIgnoreDups";
+            this.checkBoxIgnoreDups.Size = new System.Drawing.Size(82, 17);
+            this.checkBoxIgnoreDups.TabIndex = 11;
+            this.checkBoxIgnoreDups.Text = "Ignore dups";
+            this.checkBoxIgnoreDups.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 389);
+            this.Controls.Add(this.checkBoxIgnoreDups);
+            this.Controls.Add(this.checkBoxIgnoreMode);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.run);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ADIFCompare v1.5.1 W9MDB";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBoxIgnoreMode;
+        private System.Windows.Forms.CheckBox checkBoxIgnoreDups;
+    }
+}
+
---
+++ ADIFCompare/Form1.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADIFCompare
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<String, int> list1 = new Dictionary<string, int>();
+        Dictionary<String, int> list2 = new Dictionary<string, int>();
+        Dictionary<String, int> listchk = new Dictionary<string, int>();
+        int listchkTotal = 0;
+        String filename1 = "Unknown";
+        String filename2 = "Unknown";
+        String callsignFilter = "";
+        public Form1()
+        {
+            InitializeComponent();
+            /*
+            listchk.Add("20150209/22:?? 10M JT65 KE7LHT",0);
+            listchk.Add("20141113/13:?? 40M JT65 N2VWP", 0);
+            listchk.Add("20140810/12:?? 40M JT65 N2VQ", 0);
+            listchk.Add("20140304/15:?? 10M JT65 PD9HJ", 0);
+            listchk.Add("20140125/14:?? 40M JT65 VE3LFS", 0);
+            listchk.Add("20140118/21:?? 17M JT65 W2KWO", 0);
+            listchk.Add("20140112/13:?? 15M JT65 NR9Q", 0);
+            listchk.Add("20140108/15:?? 15M JT65 KS2F", 0);
+            listchk.Add("20140101/18:?? 17M JT65 DE9ZZZ", 0);
+            listchk.Add("20131122/22:?? 15M JT65 JA1KXQ", 0);
+            listchk.Add("20131122/11:?? 80M JT65 W9ATB", 0);
+            listchk.Add("20131117/13:?? 40M JT65 W9ATB", 0);
+            listchk.Add("20131115/14:?? 40M JT65 N9OJC", 0);
+            listchk.Add("20131109/14:?? 20M JT65 KJ4RKB", 0);
+            listchk.Add("20160119/14:?? 40M JT65 KM5LY", 0);
+            listchk.Add("20151226/15:?? 20M JT65 KK4UQ", 0);
+            listchk.Add("20150922/16:?? 15M JT9 F1IWH", 0);
+            listchk.Add("20150724/04:?? 40M JT65 AF6PM", 0);
+            listchk.Add("20150601/21:?? 20M JT9 UT7IS", 0);
+            listchk.Add("20150529/20:?? 17M JT9 F5RRS", 0);
+            listchk.Add("20150403/04:?? 20M JT65 KD7VDB", 0);
+            listchk.Add("20150111/13:?? 40M JT9 K9HIO", 0);
+            listchk.Add("20140701/16:?? 15M JT65 AE7JP", 0);
+            listchk.Add("20140306/15:?? 10M JT65 HA5WA", 0);
+            listchk.Add("20140101/13:?? 17M JT65 KN3A", 0);
+            listchk.Add("20131222/19:?? 10M JT65 KG7AV", 0);
+            listchk.Add("20131028/14:?? 20M JT65 WA7AG", 0);
+            listchk.Add("20131019/15:?? 20M JT65 NA6L", 0);
+            listchk.Add("20131215/20:?? 17M JT65 VA3WLD", 0);
+            */
+            /*
+            20140118 / 21:?? 17M  JT65 W2KWO  Duplicate? Why is this a dup?
+            */
+            
+        }
+
+        int Digits(String s)
+        {
+            int n = 0;
+            char[] array = s.ToCharArray();
+            for(n=0;n<array.Count();++n)
+            {
+                char c = array[n];
+                if (c >= '0' && c <= '9') ++n;
+            }
+            return n;
+        }
+
+        void AddToList(String filename, Dictionary<String, int> list, Label label)
+        {
+            String line = "";
+            String tmpline = "";
+            String call = "";
+            String qsodate = "";
+            String timeon = "";
+            String band = "";
+            String mode = "";
+            String stationCallSign = "";
+            bool eqsl_qsl_rcvd = false;
+            bool lotw_qsl_rcvd = false;
+            bool qsl_rcvd = false;
+            Char[] delims = { '>', ':' };
+            int nlines = 0;
+            int ndups = 0;
+            StreamWriter writer = new StreamWriter(filename+".txt");
+            Stopwatch timer1 = new Stopwatch();
+            using (var reader = File.OpenText(filename))
+            {
+                timer1.Start();
+                while ((tmpline = reader.ReadLine()) != null)
+                {
+
+                    if (reader.EndOfStream && tmpline.Length == 0)
+                    {
+                        continue;
+                    }
+                    if (tmpline.Length < 3) continue;
+                    if (tmpline.Substring(0,1)=="#") continue;
+                    if (tmpline.Contains("ADIF_VERS:")) continue;
+                    line += tmpline;
+                    if (!line.Contains("<EOR") && !line.Contains("<eor")) continue;
+                    //richTextBox1.AppendText(line);
+                    if (line.Contains("<CALL:")||line.Contains("<call:"))
+                    {
+                        //continue;
+                        //Regex r = new Regex(@"", RegexOptions.None, TimeSpan.FromMilliseconds(150));
+                        //if (line.Contains("NY1S"))
+                        //    richTextBox1.AppendText("NY1S");
+                        //int nn = 0;
+                        //if (line.Contains("W1AW/7"))
+                        //{
+                        //    ++nn;
+                        //}
+                        String[] tokens = line.Split('<');
+                        int n = 0;
+
+                        foreach (String s in tokens)
+                        {
+                            String[] tokens3 = s.ToUpper().Split(':');
+                            String keyword = "";
+                            if (tokens3.Length > 0) keyword = tokens3[0];
+                            if (keyword.Equals("CALL"))
+                            {
+                                ++n;
+                                String[] tokens4 = s.Split(delims);
+                                call = tokens4[2].TrimStart().TrimEnd();
+                                ++nlines;
+                                if ((nlines%100)==0) label.Text = nlines.ToString();
+                                Application.DoEvents();
+                            }
+                            else if (keyword.Equals("STATION_CALLSIGN"))
+                            {
+                                //++n;
+                                String[] tokens4 = s.Split(delims);
+                                stationCallSign = tokens4[2].TrimStart().TrimEnd();
+                            }
+                            else if (keyword.Equals("TIME_ON"))
+                            {
+                                ++n;
+                                String[] tokens5 = s.Split(delims);
+                                timeon = tokens5[2].TrimStart().Substring(0, 4);
+                                //int hour = int.Parse(timeon.Substring(0,2));
+                                //int min  = int.Parse(timeon.Substring(2,2));
+                                //int whichMinute = 0;
+                                //if (min >= 30) whichMinute = 1;
+                                //else whichMinute = 0;
+                                //timeon = (hour*100 + whichMinute).ToString();
+                            }
+                            else if (keyword.Equals("BAND"))
+                            {
+                                ++n;
+                                String[] tokens6 = s.Split(delims);
+                                band = tokens6[2].TrimStart().ToUpper().TrimEnd();
+                            }
+                            else if (keyword.Equals("MODE"))
+                            {
+                                ++n;
+                                String[] tokens7 = s.Split(delims);
+                                mode = tokens7[2].TrimStart().ToUpper().TrimEnd();
+                                if (mode.Contains("JT65")) mode = "JT65";
+                                if (mode.Contains("PSK")) mode="PSK";
+                                if (checkBoxIgnoreMode.Checked) mode = "ANY";
+                            }
+                            else if (keyword.Equals("QSO_DATE"))
+                            {
+                                ++n;
+                                String[] tokens8 = s.Split(delims);
+                                int ntoken = 2;
+                                if (tokens8[ntoken].Equals("D"))
+                                {
+                                    ++ntoken;
+                                }
+                                qsodate = tokens8[ntoken].TrimStart().TrimEnd();
+                            }
+                            /*
+                            if (keyword.Contains("QSL_RCVD"))
+                            {
+                                String[] tokens9 = s.Split(delims);
+                                 String who = tokens9[0].ToUpper();
+                                if (who.Contains("LOTW"))
+                                {
+                                    lotw_qsl_rcvd = tokens9[2].ToUpper().Trim().Equals("Y") || tokens9[2].ToUpper().Trim().Equals("V");
+
+                                }
+                                else if (who.Contains("EQSL"))
+                                {
+                                    eqsl_qsl_rcvd = tokens9[2].ToUpper().Trim().Equals("Y");
+
+                                }
+                                else
+                                {
+                                    qsl_rcvd = tokens9[2].ToUpper().Trim().Equals("Y");
+                                }
+                            }
+                            */
+                            bool for_us = true;
+                            if (callsignFilter.Length > 0 && !stationCallSign.Equals(callsignFilter)) for_us = false;
+                            if (for_us && n >= 5)
+                            {
+                                //String key = qsodate + "/" + timeon + "?\t" + band + "\t" +mode + "\t" + call + "\tlotw/eqsl="+lotw_qsl_rcvd+"/"+eqsl_qsl_rcvd;
+                                String key = qsodate + "/" + timeon + "?\t" + band + "\t" +mode + "\t" + call;
+                                //richTextBox1.AppendText(key + "\n");
+                                //break;
+                                try
+                                {
+                                    writer.WriteLine(key);
+                                    if (!list.ContainsKey(key))
+                                    {
+                                        list.Add(key, 0);
+                                    }
+                                    else if (!checkBoxIgnoreDups.Checked) {
+                                        richTextBox1.AppendText(key + " Duplicate?\n");
+                                        ++ndups;
+                                    }
+                                }
+                                catch
+                                {
+                                    listchkTotal++;
+                                }
+                                n = 0;
+                            }
+                        }
+                    }
+                    if (line.Contains("<EOR>") || line.Contains("<eor>"))
+                    {
+                        line = "";
+                    }
+                }
+            }
+            writer.Close();
+            timer1.Stop();
+            richTextBox1.AppendText("Elapsed " + timer1.ElapsedMilliseconds / 1000.0+"\n");
+            label.Text = nlines.ToString();
+            richTextBox1.AppendText(ndups + " duplicates to check\n");
+            richTextBox1.AppendText("Lines: " + nlines + "\n");
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            list1.Clear();
+            list2.Clear();
+            run.Text = "Running";
+            run.Enabled = false;
+            //String file1 = "/Users/Mike/Dropbox/Ham/LogOM_W9MDB20161226.adi";
+            //String file2 = "/Users/Mike/Downloads/w9mdb.5824.20161226211542.adi";
+            String file1 = textBox1.Text;
+            String file2 = textBox2.Text;
+            richTextBox1.AppendText("Processing " + file1 + "\n");
+            Application.DoEvents();
+            Char[] delimiters = { '.', '_',' ' };
+            filename1 = file1.Split('\\').Last();
+            filename1 = filename1.Split(delimiters)[0];
+            filename2 = file2.Split('\\').Last();
+            filename2 = filename2.Split(delimiters)[0];
+            callsignFilter = textBox3.Text;
+
+            AddToList(file1, list1,label1);
+            richTextBox1.AppendText("===============================\n");
+            richTextBox1.AppendText("Processing " + file2 + "\n");
+            Application.DoEvents();
+            AddToList(file2, list2,label2);
+            richTextBox1.AppendText("===============================\n");
+            richTextBox1.AppendText("Extra QSOs in "+file1 + "\n");
+            int n1 = 0;
+            foreach (KeyValuePair<String, int> pair in list1)
+            {
+                Application.DoEvents();
+                if (!list2.ContainsKey(pair.Key))
+                {
+                    Application.DoEvents();
+                    ++n1;
+                    richTextBox1.AppendText(filename1+" has\t" + pair.Key + "\n");
+                }
+            }
+            richTextBox1.AppendText(n1 + " entries need checking\n");
+            richTextBox1.AppendText("===============================\n");
+            richTextBox1.AppendText("Extra QSOs in " + file2 + "\n");
+            int n2 = 0;
+            foreach (KeyValuePair<String, int> pair in list2)
+            {
+                //if (!list1.ContainsKey(pair.Key) && !pair.Key.Contains("SWL") && digits(pair.Key) < 3)
+                if (!list1.ContainsKey(pair.Key))
+                {
+                    Application.DoEvents();
+                    richTextBox1.AppendText(filename2+" has\t" + pair.Key + "\n");
+                    ++n2;
+                }
+            }
+            richTextBox1.AppendText(n2 + " entries need checking\n");
+            richTextBox1.AppendText("===============================\n");
+            //richTextBox1.AppendText(file1 + " has " + n1 + " potential errors");
+            //richTextBox1.AppendText(file2 + " has " + n2 + " potential errors");
+            //richTextBox1.AppendText(listchk.Count + " entries excluded from check\n");
+            richTextBox1.AppendText(file1 + " has " + list1.Count + " uniqie entries\n");
+            richTextBox1.AppendText(file2 + " has " + list2.Count + " unique entries\n");
+            //richTextBox1.AppendText(file1 + " total entries should be " + (list1.Count + listchkTotal)+"\n");
+            //richTextBox1.AppendText(file2 + " total entries should be " + (list2.Count + listchkTotal)+"\n");
+            run.Text = "Run";
+            run.Enabled = true;
+        }
+
+        private String GetFileName()
+        {
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
+                Multiselect = false,
+                Filter = "ADIF Files (*.adi)|*.adi|All files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                return dialog.FileName;
+            }
+            return "";
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            String filename = GetFileName();
+            if (filename.Length > 0)
+            {
+                textBox1.Text = filename;
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            String filename = GetFileName();
+            if (filename.Length > 0)
+            {
+                textBox2.Text = filename;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
